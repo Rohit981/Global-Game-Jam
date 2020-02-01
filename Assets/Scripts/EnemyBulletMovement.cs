1da287ee9e90cc7c25f8edc7d9930ea92ba107c1
Rohit <rohitpathak987@gmail.com>
2020-02-01 16:41:09 +0000

    Added Enemies shooting

--- Assets/Scripts/EnemyBulletMovement.cs
+++ Assets/Scripts/EnemyBulletMovement.cs
@@ -6,17 +6,31 @@ using UnityEngine;
 public class EnemyBulletMovement : MonoBehaviour
 {
     private Transform target;
-    Rigidbody2D rb;
+    private Rigidbody2D rb;
+    [SerializeField] private float speed;
+    private Vector3 targetVector;
     // Start is called before the first frame update
     void Start()
     {
-
+        target = GameObject.FindGameObjectWithTag("Player").transform;
         rb = GetComponent<Rigidbody2D>();
+
+        targetVector = target.position - transform.position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-      //transform.position += Vector2.MoveTowards(transform.position, )
+        
+        rb.velocity = targetVector * speed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
