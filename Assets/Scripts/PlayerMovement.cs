3b39f8b9e5a2c8aceaf126b981ed6a3709fdb7c3
aristophanesyates <56827855+aristophanesyates@users.noreply.github.com>
2020-02-01 02:45:54 +0000

    Added playermovement and ladders

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,66 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rigidBody;
+    private bool onLadder;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = gameObject.GetComponent<Rigidbody2D>();
+        onLadder = false;
     }
 
+   
+
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (onLadder)
+        {
+            rigidBody.gravityScale = 0;
+            if (Input.GetKey("w"))
+            {
+                //rigidBody.AddForce(new Vector2(-5f, 0f), new ForceMode2D());
+                rigidBody.transform.Translate(new Vector2(0f, 0.5f * Time.deltaTime));
+            }
+            if (Input.GetKey("s"))
+            {
+                //rigidBody.AddForce(new Vector2(5f, 0f), new ForceMode2D());
+                rigidBody.transform.Translate(new Vector2(0f, -0.5f * Time.deltaTime));
+            }
+        }
+        else
+        {
+            rigidBody.gravityScale = 0.5f;
+
+        }
+        if (Input.GetKey("a"))
+        {
+            //rigidBody.AddForce(new Vector2(-5f, 0f), new ForceMode2D());
+            rigidBody.transform.Translate(new Vector2(-0.5f * Time.deltaTime, 0f));
+        }
+        if (Input.GetKey("d"))
+        {
+            //rigidBody.AddForce(new Vector2(5f, 0f), new ForceMode2D());
+            rigidBody.transform.Translate(new Vector2(0.5f * Time.deltaTime, 0f));
+        }
+    }
+
+    
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Ladder")
+        {
+            onLadder = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Ladder")
+        {
+            onLadder = false;
+        }
     }
 }
---
+++ Assets/Scripts/testPunt.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testPunt : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("z"))
+        {
+            //rigidBody.AddForce(new Vector2(-5f, 0f), new ForceMode2D());
+            gameObject.GetComponent<Rigidbody2D>().transform.Translate(new Vector2(-5f * Time.deltaTime, 0f));
+            gameObject.GetComponent<Rigidbody2D>().transform.Translate(new Vector2(0f, -5f * Time.deltaTime));
+        }
+        if (Input.GetKey("q"))
+        {
+            //rigidBody.AddForce(new Vector2(5f, 0f), new ForceMode2D());
+            gameObject.GetComponent<Rigidbody2D>().transform.Translate(new Vector2(5f * Time.deltaTime, 0f));
+            gameObject.GetComponent<Rigidbody2D>().transform.Translate(new Vector2(0f, 5f * Time.deltaTime));
+        }
+    }
+}
