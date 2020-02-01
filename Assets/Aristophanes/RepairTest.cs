6b3af637f4b89977a479c9c589a899671a985e8a
aristophanesyates <56827855+aristophanesyates@users.noreply.github.com>
2020-02-01 20:22:02 +0000

    Added shield penalty and repair button spam mechanic

---
+++ Assets/Aristophanes/RepairTest.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepairTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private GameObject collidedObj;
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag != "Player") return;
+
+        if (GetComponent<ShipModuleTest>() == null) return;
+
+        collidedObj = collision.gameObject;
+        //if (Input.GetButton(collidedObj.GetComponent<PlayerMovement>().playerInteract))
+        {
+            GetComponent<ShipModuleTest>().isRepairing = true;
+            if (GetComponent<ShipModuleTest>().health < 0)
+            {
+                GetComponent<ShipModuleTest>().health = 0;
+            }
+        }
+        //else
+        //    GetComponent<ShipModuleTest>().isRepairing = false;
+        if (Input.GetButtonDown(collidedObj.GetComponent<PlayerMovement>().playerInteract))
+        {
+            GetComponent<ShipModuleTest>().neglect = 0.0001f;
+            if (GetComponent<ShipModuleTest>().amountToRepair < 10)
+            {
+                GetComponent<ShipModuleTest>().amountToRepair += GetComponent<ShipModuleTest>().repairSpeed;
+            }
+        }
+    }
+
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject != collidedObj) return;
+        GetComponent<ShipModuleTest>().isRepairing = false;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Aristophanes/ShipModuleTest.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipModuleTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] public float health = 15;
+    public bool isRepairing = false;
+    internal float damageMagnifier = 1;
+    [Range(0.0001f, 2f)] public float repairSpeed = 0.0001f;
+    [Range(0.0001f, 2f)] public float secondsTillRepairStop = 0.0001f;
+    public float neglect = 0.0001f;
+    public float amountToRepair = 0f;
+    void Start()
+    {
+
+    }
+    protected void UpdateRepairBonus()
+    {
+        neglect += Time.deltaTime;
+        if (isRepairing)
+        {
+            //amountToRepair -= secondsTillRepairStop;
+            if (amountToRepair - ((neglect / secondsTillRepairStop) * amountToRepair) < 0)
+            {
+                amountToRepair = 0f;
+            }
+        }
+        else
+        {
+            amountToRepair = 0f;
+            Debug.Log("You stopped repairing.");
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+
+    protected void Repairing()
+    {
+        if (isRepairing)
+        {
+
+            health += Time.deltaTime * (amountToRepair - ((neglect / secondsTillRepairStop) * amountToRepair));
+        }
+
+    }
+
+}
